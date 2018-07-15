1fed122bf2f823fe447280966c0c0da1d835207d
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-07-15 22:16:37 +0300

    добавил в файл gitignore папку dist, переписал сборку gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,92 +1,102 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create(),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    csso = require('gulp-csso'),
-    gulpif = require('gulp-if'),
-    spritesmith = require('gulp.spritesmith'),
-    del = require('del');
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      server = require('browser-sync').create(),
+      concat = require('gulp-concat'),
+      imagemin = require('gulp-imagemin'),
+      del = require('del'),
+      sequence = require('gulp-sequence');
 
+// Default task
+gulp.task('default', ['styles:sass', 'server']);
 
-gulp.task('styles', function () {
-   return gulp.src('src/sass/main.scss')
-       .pipe(sass().on('error', sass.logError))
-       .pipe(gulp.dest('src/css'))
-       .pipe(browserSync.reload({
-           stream: true
-       }));
+// Compile SCSS to CSS
+gulp.task('styles:sass', function() {
+    return gulp.src('src/sass/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/css/'))
+        .pipe(server.stream());
 });
 
-gulp.task('styles:min', function () {
-    return gulp.src('src/css/main.css')
-        .pipe(csso())
-        .pipe(concat('main.min.css'))
-        .pipe(gulp.dest('src/css'));
-});
-
-gulp.task('scripts', function () {
-    return gulp.src([
-        'src/scripts/common.js'
-    ])
-        .pipe(concat('common.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('src/scripts/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
-
-gulp.task('sprite:png', function () {
-    return gulp.src('src/sprite/**/*.png')
-        .pipe(spritesmith({
-            imgName: 'sprite.png',
-            cssName: 'sprite.scss',
-            imgPath: '../images/sprite.png'
-        }))
-        .pipe(gulpif('*.png', gulp.dest('src/images')))
-        .pipe(gulpif('*.scss', gulp.dest('src/sass/sprite')));
-});
-
-gulp.task('bs', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./src"
-        },
-        notify: false,
-        open: false
+gulp.task('server', ['styles:sass'], function() {
+    server.init({
+        server: "./src",
+        notify: false
     });
+
+    gulp.watch("src/sass/*.scss", ['styles:sass']);
+    gulp.watch("src/scripts/*.js", ['scripts:concat']).on('change', server.reload);
+    gulp.watch("src/*.html").on('change', server.reload);
 });
 
-gulp.task('watch', function () {
-   gulp.watch('src/index.html').on('change', browserSync.reload);
-   gulp.watch('src/sass/**/*.scss', gulp.series('styles'));
-   gulp.watch('src/scripts/common.js', gulp.series('scripts'));
-
-});
-
-gulp.task('del', function () {
-   return del('./build')
-});
-
-gulp.task('copy', function () {
+// Make one script file from all
+gulp.task('scripts:concat', function() {
     return gulp.src([
-        'src/**/*.*',
-        '!src/{sass,sass/**}',
-        '!src/{sprite,sprite/**}',
-        '!src/{libs,libs/**}'
+        'src/scripts/temp.js'
     ])
-        .pipe(gulp.dest('./build'));
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('src/scripts/'));
 });
 
-gulp.task('default',
-    gulp.series(
-        gulp.parallel('styles', 'scripts'),
-        gulp.parallel('bs', 'watch')
-    )
-);
+// Make one script from all js libs
+gulp.task('scripts:libs', function() {
+    return gulp.src([
+        'src/libs/temp.js'
+    ])
+        .pipe(concat('libs.js'))
+        .pipe(gulp.dest('src/scripts/'));
+});
 
-gulp.task('build',
-  gulp.series('del', 'copy')
-);
+// Imagemin task
+gulp.task('images:min', function() {
+    return gulp.src('src/images/**/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('dist/images/'));
+});
 
+// Copy scripts
+gulp.task('scripts:copy', function() {
+    gulp.src([
+        'src/scripts/script.js',
+        'src/scripts/libs.js'
+    ])
+        .pipe(gulp.dest('dist/scripts/'));
+});
+
+// Copy styles
+gulp.task('styles:copy', function() {
+    gulp.src('src/css/style.css')
+        .pipe(gulp.dest('dist/css/'));
+});
+
+// Copy html
+gulp.task('html:copy', function() {
+    gulp.src('src/**/*.html')
+        .pipe(gulp.dest('dist/'));
+});
+
+// Copy fonts
+gulp.task('fonts:copy', function() {
+    gulp.src('src/fonts/**')
+        .pipe(gulp.dest('dist/fonts/'));
+})
+
+// Delete dist folder
+gulp.task('del', function() {
+    return del.sync('dist');
+})
+
+// Copy files
+gulp.task('files:copy', sequence('html:copy', 'styles:copy', 'scripts:copy', 'images:min', 'fonts:copy'));
+
+// Build task
+gulp.task('build', sequence('del', 'files:copy'));
